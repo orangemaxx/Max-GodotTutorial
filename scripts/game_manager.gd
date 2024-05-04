07c9b3a26f62ba0ee3553c98e71017c265d6401a
Max H <49844637+orangemaxx@users.noreply.github.com>
2024-05-05 09:58:25 +1200

    i made a dlc!!!

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -4,6 +4,13 @@ extends Node
 
 var score = 0
 
+func _ready():
+	var sucsess = ProjectSettings.load_resource_pack("res://dlc.pck")
+	
+	if sucsess:
+		get_tree().change_scene_to_file("res://scenes/maxdlc/dlc.tscn")
+
+
 func add_point():
 	score += 1
 	score_label.text = "You've collected " + str(score) + " coins!"
